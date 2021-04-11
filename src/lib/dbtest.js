61718542d3fa9ede3a66e8220796232302d3264b
magnus-hansson <hanssmag@gmail.com>
2021-04-11 14:53:42 +0200

    nouri

--- src/lib/dbtest.js
+++ src/lib/dbtest.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 console.log(process.env);
 const uri =
-	'mongodb+srv://garage:CUZxg5qk3fm7ZRg@cluster0.rp737.mongodb.net/test?retryWrites=true&w=majority';
+	
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 async function run() {
